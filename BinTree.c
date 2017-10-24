b9f01fb6e4261dc8032821a40cb23f9a6a61a01c
SCarmo <scarmody@tcd.ie>
2017-10-24 13:41:44 +0100

    Branch test

--- BinTree.c
+++ BinTree.c
@@ -1,3 +1,7 @@
+// THIS COMMENT SHOULD BE IN THE NEW BRANCH
+
+
+
 #include<stdlib.h>
 #include<stdio.h>
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
